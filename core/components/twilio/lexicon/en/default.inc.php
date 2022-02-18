4ee9b149cf97901cbb05dd3631e76d4480dd2b6a
matdave <mat@matdave.com>
2022-02-18 15:30:27 -0600

    - fix lexicon entries - bump version

--- core/components/twilio/lexicon/en/default.inc.php
+++ core/components/twilio/lexicon/en/default.inc.php
@@ -9,9 +9,9 @@
 
 $_lang['twilio'] = 'Twilio';
 // Settings
-$_lang['twilio.account_sid'] = 'Account SID';
-$_lang['twilio.account_sid_desc'] = 'Found under Account Info here https://console.twilio.com/';
-$_lang['twilio.account_token'] = 'Account Auth Token';
-$_lang['twilio.account_token_desc'] = 'Found under Account Info here https://console.twilio.com/';
-$_lang['twilio.service_id'] = 'Service ID';
-$_lang['twilio.service_id_desc'] = 'Found under Services Page here https://console.twilio.com/us1/develop/verify/services';
+$_lang['setting_twilio.account_sid'] = 'Account SID';
+$_lang['setting_twilio.account_sid_desc'] = 'Found under Account Info here https://console.twilio.com/';
+$_lang['setting_twilio.account_token'] = 'Account Auth Token';
+$_lang['setting_twilio.account_token_desc'] = 'Found under Account Info here https://console.twilio.com/';
+$_lang['setting_twilio.service_id'] = 'Service ID';
+$_lang['setting_twilio.service_id_desc'] = 'Found under Services Page here https://console.twilio.com/us1/develop/verify/services';
