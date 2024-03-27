f150314df27890aedba3e8dfc0b659ad01346c8d
Mat Dave Jones <mat@matdave.com>
2024-03-27 16:46:25 -0500

    Fix broken user page

--- assets/components/twilio/js/mgr/helpers/qr.js
+++ assets/components/twilio/js/mgr/helpers/qr.js
@@ -1,10 +1,10 @@
 Ext.onReady(function() {
-    var div = document.getElementById('modx-panel-profile-update');
-    div.innerHTML = div.innerHTML + '<div class="x-form-item x-tab-item x-form-element">'
+    var div = Ext.get('modx-panel-profile-update');
+    Ext.DomHelper.insertAfter(div, '<div class="x-form-item x-tab-item x-form-element">'
         + '<label for="qrcode" style="width:auto;" class="x-form-item-label">'+
         twilio.qrText
         +':\n\</label></div>'
-        + '<div id="qrcode"><img id="qrimg" src=""></div>';
+        + '<div id="qrcode"><img id="qrimg" src=""></div>');
     MODx.Ajax.request({
         url: MODx.config.connector_url,
         params:{action:'MODX\\Twilio\\Processors\\TOTP\\QR', user: twilio.config.user.user},
